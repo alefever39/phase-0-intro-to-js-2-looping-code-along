54881c8928dd4fde59280ca8675135bc74880ad4
Allie LeFever <alefever39@gmail.com>
2022-05-18 17:32:58 -0600

    commit tree 4989ab4c4590551db04715f19f2c2f323482935d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const cardsWritten = []
+    for (let i=0; i < namesArray.length; i++) {
+        const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`
+        cardsWritten.push(message)
+    }
+    return cardsWritten
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int)
+        int--
+    }
+}
